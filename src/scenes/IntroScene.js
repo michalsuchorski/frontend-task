02eecf865cb408b32a53fec7c7dce1f64993810b
Michal Suchorski <michal.suchorski7@gmail.com>
2025-07-01 13:55:22 +0200

    finish scene 1 style

--- src/scenes/IntroScene.js
+++ src/scenes/IntroScene.js
@@ -12,41 +12,56 @@ export class IntroScene {
   }
 
   init() {
-    const headline = document.querySelector('.scene-intro-content-image');
-    const introTextEl = document.querySelector('.scene-intro-content-text-wrapper');
-    const allTiles = document.querySelectorAll('.scene-intro-content-panel-plate');
-    const allTilesText = document.querySelectorAll('.scene-intro-content-panel-plate-text')
+    const headline = document.querySelector(".scene-intro-content-image");
+    const introTextEl = document.querySelector(".intro-text");
+    const allTiles = document.querySelectorAll(
+      ".scene-intro-content-panel-plate"
+    );
+    const allTilesText = document.querySelectorAll(
+      ".scene-intro-content-panel-plate-text"
+    );
+    const aboutUsEl = document.querySelector(".about-us-element");
+    const shadowEl = document.querySelector(".shadow-wrapper");
 
     gsap.from(allTiles, {
-        duration: 0.5,
-        opacity: 0,
-        y: 50,
-        stagger: 0.1,
-        ease: "power2.out",
-    })
+      duration: 0.5,
+      opacity: 0,
+      y: 50,
+      stagger: 0.1,
+      ease: "power2.out",
+    });
 
     gsap.from(allTilesText, {
-        duration: 0.5,
-        opacity: 0,
-        x: 20,
-        stagger: 0.1,
-        ease: "back.inOut"
-    })
+      duration: 0.5,
+      opacity: 0,
+      x: 20,
+      stagger: 0.1,
+      ease: "back.inOut",
+    });
 
     gsap.from(headline, {
-        duration: 1,
-        y: '-100%'
-    })
+      duration: 1,
+      y: "-100%",
+    });
 
     gsap.from(introTextEl, {
-        duration: 1,
-        x: '-100%'
-    })
+      duration: 0.7,
+      y: "-100%",
+    });
+
+    gsap.from(aboutUsEl, {
+      duration: 0.7,
+      y: "100%",
+    });
+    gsap.from(shadowEl, {
+      duration: 1,
+      y: "100%",
+    });
 
     this.timeout = setTimeout(() => {
       this.scrollToGallery();
       this.onComplete?.();
-    }, 222000);
+    }, 8000);
   }
   scrollToGallery() {
     if (!this.scrollTarget) return;
